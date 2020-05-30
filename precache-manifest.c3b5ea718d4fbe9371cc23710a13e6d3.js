621f3892961ae7dcdafd3cf4583c1dcf77f3e07f
andrey_raut <andrey.raut@fusion-team.com>
2020-05-30 03:40:48 +0300

    Updates

---
+++ precache-manifest.c3b5ea718d4fbe9371cc23710a13e6d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05872979e912d6d804dac695a528bc03",
+    "url": "/redux-reducer-test/index.html"
+  },
+  {
+    "revision": "94aa1c6caae1baf0fbe8",
+    "url": "/redux-reducer-test/static/js/2.88113188.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/redux-reducer-test/static/js/2.88113188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4efa700f942e9bc1f7",
+    "url": "/redux-reducer-test/static/js/main.0000534b.chunk.js"
+  },
+  {
+    "revision": "444c55b9a97dbb96669d",
+    "url": "/redux-reducer-test/static/js/runtime-main.e502360d.js"
+  }
+]);
